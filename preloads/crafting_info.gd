ce48f9c4981293957061eee837d8562f231dfc07
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-04-13 11:04:34 +0100

    Singleton with neutral crafting information like resource enums, bench types, and resource names.

---
+++ preloads/crafting_info.gd
@@ -0,0 +1,26 @@
+extends Node
+
+enum Benches{
+	None,
+	Crafting_Bench
+}
+
+enum Resources {
+	Glass,
+	Cloth,
+	Metal,
+	Electonics,
+	Explosives
+}
+
+const RESOURCE_NAMES:Dictionary ={
+	Resources.Glass: "Glass",
+	Resources.Cloth: "Cloth",
+	Resources.Metal: "Metal",
+	Resources.Electonics: "Electonics",
+	Resources.Explosives: "Explosives",
+} 
+
+var recipes: Dictionary = {
+	bruh = true
+}
