d979259e8980893dcb5269c24aa27253821cba07
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-04-13 11:03:27 +0100

    Added crafting recipe resource which has a requirement, cost, and output. Created two test recipes.

---
+++ crafting/recipes/crafting_recipe.gd
@@ -0,0 +1,40 @@
+extends Resource
+class_name CraftingRecipe
+
+
+@export_category("Requirements")
+@export var bench_requirement: CraftingInfo.Benches
+@export_category("Resource costs")
+@export var glass: int
+@export var cloth: int
+@export var metal: int
+@export var electronics: int
+@export var explosives: int
+@export_category("Crafting Output")
+@export var output_item:TrapData
+
+#None of this is pretty :(
+#TODO Clean both these up maybe?
+func can_craft(crafting_resources:Dictionary,bench:CraftingInfo.Benches):
+	if bench != bench_requirement:
+		return false
+	if crafting_resources[CraftingInfo.Resources.Glass]<glass:
+		return false
+	if crafting_resources[CraftingInfo.Resources.Cloth]<cloth:
+		return false
+	if crafting_resources[CraftingInfo.Resources.Metal]<metal:
+		return false
+	if crafting_resources[CraftingInfo.Resources.Electonics]<electronics:
+		return false
+	if crafting_resources[CraftingInfo.Resources.Explosives]<explosives:
+		return false
+	return true
+
+func craft(crafting_resources:Dictionary,bench:CraftingInfo.Benches) -> TrapData:
+	if !can_craft(crafting_resources,bench): return null
+	crafting_resources[CraftingInfo.Resources.Glass] -= glass
+	crafting_resources[CraftingInfo.Resources.Cloth] -= cloth
+	crafting_resources[CraftingInfo.Resources.Metal] -= metal
+	crafting_resources[CraftingInfo.Resources.Electonics] -= electronics
+	crafting_resources[CraftingInfo.Resources.Explosives] -= explosives
+	return output_item
