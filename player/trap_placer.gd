70da9173fcb1d87b87d468247e5e51069c11853c
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-04-13 10:55:27 +0100

    Removed inventory and input logic from the trap placer script. Extracted placing logic into functions.

--- player/trap_placer.gd
+++ player/trap_placer.gd
@@ -1,36 +1,34 @@
 extends Node3D
 
 @onready var ray_cast = $RayCast3D
-@onready var inventory_manager = $InventoryManager
 
-
-var placing: bool = false
 var current_trap
 
+func is_placing():
+	return current_trap != null
 
 func _process(delta):
-	if !placing:
-		return
-	if !ray_cast.is_colliding():
-		return
+	if !current_trap or !ray_cast.is_colliding(): return
 	current_trap.global_position = ray_cast.get_collision_point()+current_trap.place_offset
 
-func _input(event):
-	if event.is_action_pressed("next_item") and !placing:
-		inventory_manager.next_item()
-	if event.is_action_pressed("prev_item") and !placing:
-		inventory_manager.prev_item()
-	
-	if !event.is_action_pressed("player_place_trap"):
-		return
-	
-	if placing:
+func start_placing(trap_data:TrapData):
+	if !trap_data: return
+	if current_trap:
+		cancel_placement()
+	current_trap = trap_data.get_instance()
+	get_tree().get_first_node_in_group("world").add_child(current_trap)
+
+func place_trap() -> bool:
+	if !current_trap: return false
+	if current_trap.can_place():
 		current_trap.place()
 		current_trap = null
-		placing = false
-		inventory_manager.remove_selected()
+		return true
 	else:
-		if !inventory_manager.get_selected_item():return
-		current_trap = inventory_manager.get_selected_item().get_instance()
-		get_tree().get_first_node_in_group("world").add_child(current_trap)
-		placing = true
+		cancel_placement()
+	return false
+
+func cancel_placement():
+	if !current_trap: return
+	current_trap.queue_free()
+	current_trap = null
