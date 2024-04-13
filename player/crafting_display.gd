7bf1bdbe0942eeb07342ed6b55212ffce1fb2a7c
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-04-13 11:41:02 +0100

    Can now display selected recipe.

--- player/crafting_display.gd
+++ player/crafting_display.gd
@@ -6,3 +6,7 @@ func _on_crafting_manager_resources_updated(resources):
 	for key in resources:
 		list += str(CraftingInfo.RESOURCE_NAMES[key])+" "+str(resources[key])+"\n"
 	set_text(list)
+
+
+func _on_crafting_manager_item_selected(item):
+	set_text(item.title)
