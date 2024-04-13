730561ada7177410ce3d2235e72e67f3fe3833dc
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-04-13 11:38:28 +0100

    All recipes are loaded from the recipes folder.

--- preloads/crafting_info.gd
+++ preloads/crafting_info.gd
@@ -21,6 +21,22 @@ const RESOURCE_NAMES:Dictionary ={
 	Resources.Explosives: "Explosives",
 } 
 
-var recipes: Dictionary = {
-	bruh = true
-}
+var recipes: Array
+
+func _ready():
+	load_recipes()
+
+func load_recipes():
+	recipes = load_files("res://crafting/recipes/","tres")
+	for recipe in recipes:
+		if !(recipe is CraftingRecipe):
+			recipes.erase(recipe)
+
+func load_files(path:String,type:String):
+	var resources = []
+	var folder = DirAccess.open(path)
+	for file_path in folder.get_files():
+		if !type in file_path:
+			continue
+		resources.append(load(path+file_path))
+	return resources
