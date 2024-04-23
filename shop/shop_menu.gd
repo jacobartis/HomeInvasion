0cd3cf44f68a740709a7cdfdfb9ff127b089e3da
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-04-23 23:18:50 +0100

    Removed 2nd button functionality.

--- shop/shop_menu.gd
+++ shop/shop_menu.gd
@@ -2,13 +2,12 @@ extends Control
 class_name Shop
 
 const TEST_TRAP = preload("res://traps/test_trap/test_trap.tres")
-const TEST_TRAP_2 = preload("res://traps/test_trap/test_trap2.tres")
 
 func _on_buy():
 	get_tree().get_first_node_in_group("InventoryManager").add_item(TEST_TRAP)
 
 func _on_buy2():
-	get_tree().get_first_node_in_group("InventoryManager").add_item(TEST_TRAP_2)
+	get_tree().get_first_node_in_group("InventoryManager").add_item(TEST_TRAP)
 
 func _on_draw():
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
