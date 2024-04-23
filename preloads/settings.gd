47259b70ac45495956cbadce141ccf03d5965127
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-04-23 23:24:43 +0100

    No longer locks mouse on start.

--- preloads/settings.gd
+++ preloads/settings.gd
@@ -5,10 +5,6 @@ var sensitivity: float = .004: get=get_sensitivity
 func get_sensitivity():
 	return sensitivity
 
-
-func _ready():
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-
 func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		if Input.mouse_mode==Input.MOUSE_MODE_CAPTURED:
