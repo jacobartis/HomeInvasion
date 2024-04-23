1bef82f12213e6e2400e386d5c719876c184fb69
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-04-23 23:20:57 +0100

    Created a simple main menu with limited functionality.

---
+++ menus/main_menu/main_menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+func _on_play_pressed():
+	get_tree().change_scene_to_packed(preload("res://world.tscn"))
+
+func _on_settings_pressed():
+	$Settings.show()
+	$AudioStreamPlayer.play()
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+
+func _on_funny_pressed():
+	$AudioStreamPlayer.play()
