c8d5049995ad2df6cebe85b911c36e6e9eb1a3c9
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-04-23 23:21:39 +0100

    Added get current recipe and add quant to resource.

--- player/crafting_manager.gd
+++ player/crafting_manager.gd
@@ -19,11 +19,17 @@ func set_resource_quant(resource, quant:int):
 	_resource_quant[resource] = quant
 	emit_signal("resources_updated",_resource_quant)
 
+func add_resource_quant(resource,quant):
+	set_resource_quant(resource,_resource_quant[resource]+quant)
+
 func get_resource_quant(resource:CraftingInfo.Resources):
 	return _resource_quant[resource]
 
 func get_recipes():
 	return CraftingInfo.recipes
+
+func get_selected_recipe():
+	return get_recipes()[position]
 
 func craft_selected():
 	if temp_garbage(): return
