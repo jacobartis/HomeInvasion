2b3c08f0f09bc123ea9ea46f23283c5f535fad18
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-04-24 13:13:07 +0100

    Renamed the crafting display to better respresent its funtion.

--- player/crafting_resource_display.gd
+++ player/crafting_resource_display.gd
@@ -1,12 +1,8 @@
 extends Label
 
-
 func _on_crafting_manager_resources_updated(resources):
 	var list = ""
 	for key in resources:
 		list += str(CraftingInfo.RESOURCE_NAMES[key])+" "+str(resources[key])+"\n"
 	set_text(list)
 
-
-func _on_crafting_manager_item_selected(item):
-	set_text(item.title)
