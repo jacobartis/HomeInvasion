d4e04fdffdcf5ae4395ebc0e4476261d3ff77d27
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-04-28 16:01:21 +0100

    Added interaction point node, replaces exit position variable. Player now moved to layer 3 so they can be picked up by rooms.

--- hiding_spot/hiding_spot.gd
+++ hiding_spot/hiding_spot.gd
@@ -1,11 +1,14 @@
 extends StaticBody3D
 
-@export var exit_pos:Vector3 = Vector3(0,0,-2)
+@onready var interaction_spot = $InteractionSpot
 
 var entered: bool = false
 var body: CharacterBody3D = null
 var layer: int = 0
 var mask: int = 0
+
+func get_interaction_spot_pos():
+	return interaction_spot.global_position
 
 func interact(interactor):
 	if !interactor.is_in_group("player"):return
@@ -18,14 +21,14 @@ func interact(interactor):
 func enter():
 	layer = body.get_collision_layer()
 	mask = body.get_collision_mask()
-	body.collision_layer = 0
-	body.collision_mask = 0
+	body.collision_layer = 4
+	body.collision_mask = 4
 	body.global_position = self.global_position
 	body.hiding_spot = self
 	print("enter")
 
 func exit():
-	body.global_position = self.global_position+exit_pos
+	body.global_position = self.global_position+interaction_spot.position
 	body.collision_layer = layer
 	body.collision_mask = mask
 	body.hiding_spot = null
@@ -33,5 +36,7 @@ func exit():
 	print("exit")
 
 func search():
+	$AnimationPlayer.play("search")
+	print("Searched")
 	if body:
 		exit()
