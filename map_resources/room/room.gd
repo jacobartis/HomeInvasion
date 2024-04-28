9bd2639251bc38262751351d00072c103fd0d851
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-04-28 16:04:22 +0100

    Tracks total player entries. Chaged layer to pickup hiding players. Sets current room for players.

--- map_resources/room/room.gd
+++ map_resources/room/room.gd
@@ -3,6 +3,8 @@ class_name Room
 
 var room_name: String = "default"
 
+var player_entries: int = 0
+
 @export var properties: Dictionary :
 	get:
 		return properties
@@ -18,7 +20,17 @@ func update_properties():
 func _ready():
 	add_to_group("room")
 	connect("body_entered",body_entered)
+	collision_mask = 31
+	collision_layer = 31
+
+func get_hiding_spots():
+	return get_overlapping_bodies().filter(
+		func(body):
+			return body.is_in_group("hiding_spot")
+	)
 
 func body_entered(body:Node3D):
-	if !body.is_in_group("enemy"): return
-	body.current_room = self
+	if !(body.is_in_group("enemy") or body.is_in_group("player")): return
+	if body.is_in_group("player"):
+		player_entries += 1
+	body.set_room(self)
