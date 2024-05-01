f79262013851cbc63bd757ab871b6f96a22166cd
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-05-01 22:34:51 +0100

    New preload track the rooms within a level, allows for global use of that info.

---
+++ preloads/level_info.gd
@@ -0,0 +1,34 @@
+extends Node
+
+var rooms: Dictionary = {} : get=get_rooms
+
+func get_rooms() -> Dictionary:
+	return rooms
+
+func get_rooms_array() -> Array:
+	var room_array = []
+	for room in rooms.values():
+		room_array.append(room)
+	return room_array
+
+func get_id(room:Room):
+	for key in rooms.keys():
+		if rooms[key]:
+			return key
+	return -1
+
+func add_room_section(room_section:RoomSection):
+	if !rooms.has(room_section.room_id):
+		var new_room = Room.new()
+		new_room.add_section(room_section)
+		rooms[room_section.room_id] = new_room
+	else:
+		rooms[room_section.room_id].add_section(room_section)
+	print(room_section.room_id)
+	check_valid()
+
+#Removes empty rooms
+func check_valid():
+	for key in rooms.keys():
+		if !rooms[key]:
+			rooms.erase(key)
