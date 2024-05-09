687b9d79010c5ad16981e43277fc3f09d6d6d860
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-05-09 01:28:46 +0100

    Added door animation and SFX. Doors start without collision for navigation mesh.

--- map_resources/door/door.gd
+++ map_resources/door/door.gd
@@ -1,7 +1,8 @@
-@tool
 extends StaticBody3D
 
 var closed:bool = false
+var close_audio:AudioStreamPlayer3D
+var open_audio:AudioStreamPlayer3D
 
 @export var properties: Dictionary :
 	get:
@@ -14,25 +15,55 @@ var closed:bool = false
 func update_properties():
 	if 'closed' in properties:
 		if properties["closed"]:
-			close()
+			closed = true
 		else:
-			open()
+			closed = false
 
 func _ready():
+	collision_mask = 1
+	collision_layer = 1
 	add_to_group("door")
-
-func interact(body):
 	if closed:
+		call_deferred("close")
+	else:
+		call_deferred("open")
+	close_audio = AudioStreamPlayer3D.new()
+	open_audio = AudioStreamPlayer3D.new()
+	close_audio.set_stream(preload("res://map_resources/door/door_toggle.mp3"))
+	open_audio.set_stream(preload("res://map_resources/door/door_toggle.mp3"))
+	add_child(close_audio)
+	add_child(open_audio)
+	close_audio.position = Vector3.ZERO
+	open_audio.position = Vector3.ZERO
+	open_audio.set_pitch_scale(.5)
+
+func use(duration) -> void:
+	toggle()
+	await get_tree().create_timer(duration).timeout
+	toggle()
+
+func interact(body=null):
+	return
+	#toggle()
+
+func toggle():
+	if closed:
+		open_audio.play()
 		open()
 	else:
+		close_audio.play()
 		close()
 
 func open():
 	collision_layer = 2
-	hide()
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "position", global_position+Vector3(0,5,0), 1)
+	tween.play()
 	closed = false
 
 func close():
 	collision_layer = 3
-	show()
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "position", global_position-Vector3(0,5,0), .25)
+	tween.play()
 	closed = true
