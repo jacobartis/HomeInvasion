fcc3504edc7c606268cde485cf39a25b91d7e211
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-05-09 01:30:34 +0100

    Created a model and some animations for hiding spots. Added camera position node for placing the player.

--- map_resources/hidding_spot/hiding_spot.gd
+++ map_resources/hidding_spot/hiding_spot.gd
@@ -1,11 +1,27 @@
 extends StaticBody3D
 
+signal searched()
+
 @onready var interaction_spot = $InteractionSpot
 
 var entered: bool = false
 var body: CharacterBody3D = null
 var layer: int = 0
 var mask: int = 0
+var last_search: int = 0
+
+@export var properties: Dictionary :
+	get:
+		return properties
+	set(new_properties):
+		if(properties != new_properties):
+			properties = new_properties
+			update_properties()
+
+func update_properties():
+	if 'angle' in properties:
+		rotation.y = deg_to_rad(float(properties["angle"])-180)
+
 
 func get_interaction_spot_pos():
 	return interaction_spot.global_position
@@ -23,12 +39,12 @@ func enter():
 	mask = body.get_collision_mask()
 	body.collision_layer = 4
 	body.collision_mask = 4
-	body.global_position = self.global_position
+	body.global_position = $CameraPos.global_position
 	body.hiding_spot = self
 	print("enter")
 
 func exit():
-	body.global_position = self.global_position+interaction_spot.position
+	body.global_position = interaction_spot.global_position
 	body.collision_layer = layer
 	body.collision_mask = mask
 	body.hiding_spot = null
@@ -36,7 +52,8 @@ func exit():
 	print("exit")
 
 func search():
+	last_search = Time.get_ticks_msec()
+	emit_signal("searched")
 	$AnimationPlayer.play("search")
-	print("Searched")
 	if body:
 		exit()
