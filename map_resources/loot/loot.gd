5fa2abad5d3764ba2f893f375bffbfc0acac2fe7
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-05-09 01:27:21 +0100

    Added rotation to loot.

--- map_resources/loot/loot.gd
+++ map_resources/loot/loot.gd
@@ -4,7 +4,20 @@ extends StaticBody3D
 @export var loot_type: CraftingInfo.Resources
 @export var loot_quant: int
 
+@export var properties: Dictionary :
+	get:
+		return properties
+	set(new_properties):
+		if(properties != new_properties):
+			properties = new_properties
+			update_properties()
+
+func update_properties():
+	if 'angle' in properties:
+		rotation.y = deg_to_rad(float(properties["angle"])-180)
+
 var looted: bool = false
+
 
 func _ready():
 	loot_type = randi()%6
