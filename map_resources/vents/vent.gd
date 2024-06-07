a09ba07d7b4134608938fa4fd2f64057a79b66c0
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-06-07 15:54:21 +0100

    Added open animation, implemented functionality, added sound.

--- map_resources/vents/vent.gd
+++ map_resources/vents/vent.gd
@@ -1,11 +1,28 @@
 extends Node3D
+class_name Vent
 
+@export var properties: Dictionary :
+	get:
+		return properties
+	set(new_properties):
+		if(properties != new_properties):
+			properties = new_properties
+			update_properties()
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func update_properties():
+	if 'angle' in properties:
+		rotation.y = deg_to_rad(float(properties["angle"])-180)
 
+func exit(body,noise:bool):
+	$AnimationPlayer.play("open")
+	body.global_position = $InteractionArea.global_position
+	body.in_vent = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func enter(body,noise:bool):
+	$AnimationPlayer.play("open")
+	body.global_position.y = -100
+	body.in_vent = true
+
+func interact(body,args:Dictionary):
+	if body.is_in_group("enemy"):
+		enter(body,args["Noise"])
