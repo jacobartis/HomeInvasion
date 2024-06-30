59f311bd3f2976dfca130de211ad473beb31e179
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-06-30 19:21:18 +0100

    WIP emits particles.

---
+++ map_resources/particle_emitter/particle_emitter.gd
@@ -0,0 +1,13 @@
+extends GPUParticles3D
+
+@export var properties: Dictionary :
+	get:
+		return properties
+	set(new_properties):
+		if(properties != new_properties):
+			properties = new_properties
+			update_properties()
+
+func update_properties():
+	if 'angle' in properties:
+		rotation.y = deg_to_rad(float(properties["angle"]))
