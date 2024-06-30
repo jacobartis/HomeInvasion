bd7364ba5ad7566d0bb3f352e1a95da361ab54ed
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-06-30 18:52:20 +0100

    Moved icon, updated game script.

--- game/game.gd
+++ game/game.gd
@@ -1,13 +1,10 @@
 extends Node3D
 
-@onready var prep_timer = $PrepTimer
-@onready var round_timer = $RoundTimer
-
 const ENEMY = preload("res://enemy/enemy.tscn")
 
 func _ready():
-	get_tree().get_first_node_in_group("timer_display").set_display(prep_timer,"Prep:")
 	call_deferred("update_doors")
+	spawn_enemy()
 
 func update_doors():
 	for door in get_tree().get_nodes_in_group("door"):
@@ -24,16 +21,6 @@ func spawn_enemy():
 func _process(delta):
 	if Input.is_action_just_pressed("reload"):
 		get_tree().reload_current_scene()
-
-func _on_prep_timer_timeout():
-	get_tree().get_first_node_in_group("timer_display").set_display(round_timer,"Survive!:")
-	round_timer.start(99999)
-	spawn_enemy()
-
-func _on_round_timer_timeout():
-	print("you win")
-	get_tree().paused = true
-
 
 func _on_navigation_region_3d_bake_finished():
 	print("bake")
