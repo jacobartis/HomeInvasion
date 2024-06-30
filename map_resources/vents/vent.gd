63a823605c325ae6378b5a6aae54aa98135a1f07
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-06-30 19:17:01 +0100

    Created vents which the enemy can move between.

--- map_resources/vents/vent.gd
+++ map_resources/vents/vent.gd
@@ -9,20 +9,40 @@ class_name Vent
 			properties = new_properties
 			update_properties()
 
+func get_animation():
+	return "search"
+
 func update_properties():
 	if 'angle' in properties:
 		rotation.y = deg_to_rad(float(properties["angle"])-180)
 
-func exit(body,noise:bool):
-	$AnimationPlayer.play("open")
+func fake(noise:bool=true):
+	if noise:
+		$AnimationPlayer.play("open")
+	else:
+		$AnimationPlayer.play("open_silent")
+
+func exit(body,noise:bool=true, delay:float=0):
+	if noise:
+		$AnimationPlayer.play("open")
+	else:
+		$AnimationPlayer.play("open_silent")
+	await get_tree().create_timer(delay).timeout
 	body.global_position = $InteractionArea.global_position
 	body.in_vent = false
 
-func enter(body,noise:bool):
-	$AnimationPlayer.play("open")
+func enter(body,noise:bool=true, delay:float=0):
+	if noise:
+		$AnimationPlayer.play("open")
+	else:
+		$AnimationPlayer.play("open_silent")
+	await get_tree().create_timer(delay).timeout
 	body.global_position.y = -100
 	body.in_vent = true
 
-func interact(body,args:Dictionary):
+func move_sound():
+	$MoveSound.play()
+
+func interact(body,args:Dictionary={"Noise":true, "Delay":0}):
 	if body.is_in_group("enemy"):
-		enter(body,args["Noise"])
+		enter(body,args["Noise"],args["Delay"])
